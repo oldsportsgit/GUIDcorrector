4e8e77f6c9f72541850a754229d488d2df609152
a unity game developer <50415010+oldsportsgit@users.noreply.github.com>
2024-02-24 16:45:57 +0100

    Update ReplaceGUIDwithCorrectOne.cpp
    made it so that you input the paths in the console

--- ReplaceGUIDwithCorrectOne/ReplaceGUIDwithCorrectOne/ReplaceGUIDwithCorrectOne.cpp
+++ ReplaceGUIDwithCorrectOne/ReplaceGUIDwithCorrectOne/ReplaceGUIDwithCorrectOne.cpp
@@ -16,11 +16,25 @@ std::string ReplaceSubstring(std::string string, std::string substring, std::str
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::string DecompiledPackagePath = R"(C:\Users\Stanley Yelnats\Downloads\bbcrProj\ExportedProject\Assets\Scripts\Unity.RenderPipelines.Universal.Runtime)";
-    std::string ActualPackagePath = R"(C:\Users\Stanley Yelnats\Downloads\bbcrProj\ExportedProject\Library\PackageCache\com.unity.render-pipelines.universal@10.10.1\Runtime)";
-    std::string UnityProjectPath = R"(C:\Users\Stanley Yelnats\Downloads\bbcrProj\ExportedProject\Assets)";
-    for (const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator(DecompiledPackagePath))
+    std::string IncorrectGUIDsPath;
+    std::string CorrectGUIDsPath;
+    std::string UnityProjectPath;
+    std::cout << "Give the path to the Incorrect guids (the package folder containing them) usually '/Assets/Scripts'\n";
+    //std::cin >> IncorrectGUIDsPath;
+    std::getline(std::cin, IncorrectGUIDsPath);
+    std::cout << "Give the path where the actual package is that usually being '/Library/PackageCache'\n";
+    std::getline(std::cin, CorrectGUIDsPath);
+    std::cout << "Give the path to the Unity Project's 'Assets' folder '/Assets'\n";
+    std::getline(std::cin, UnityProjectPath);
+    std::filesystem::path path1 = IncorrectGUIDsPath;
+    std::filesystem::path path2 = CorrectGUIDsPath;
+    std::filesystem::path path3 = UnityProjectPath;
+    if (!std::filesystem::exists(path1) || !std::filesystem::exists(path2) || !std::filesystem::exists(path3))
+    {
+        std::cout << "WTH? a path wasn't valid! double check and try again";
+        return 0;
+    }
+    for (const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator(IncorrectGUIDsPath))
     {
         if (entry.path().extension() == ".meta")
         {
@@ -28,7 +42,7 @@ int main()
             //get file contents
             std::string contentsOfWrongGUID((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
             std::string incorrectGuid = contentsOfWrongGUID.substr(27, 32);
-            for (const auto& entry2 : std::filesystem::recursive_directory_iterator(ActualPackagePath))
+            for (const auto& entry2 : std::filesystem::recursive_directory_iterator(CorrectGUIDsPath))
             {
                 if (entry2.path().filename().string() == entry.path().filename().string())
                 {
